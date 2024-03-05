1133fe7767297af2ff5c364476bd5504604234f9
pascalniri <pascalniri@gmail.com>
2024-03-05 15:08:26 -0500

    Changed the position of menu and language dropdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import '../styles/Header.css'
-
-
+import { IoMdAdd } from "react-icons/io";
 
 class Header extends Component{
   state = {clicked: false};
@@ -13,11 +12,16 @@ class Header extends Component{
     return (
       <>
         <nav> 
-            <select className='language-one' style={{padding:'5px 10px', border:'none', outline:'none', borderRadius:'5px'}}>          
-                   <option>English</option>
-                   <option>French</option>
-            </select>         
-          <a href='#'>LOGO</a>
+        <div id='mobile' 
+              onClick=
+              {this.handleClick}
+            >
+               <i id='bar'
+                  className=
+                  {this.state.clicked ? "fas fa-times": "fas fa-bars"}
+               ></i>
+            </div>   
+          <a href='#' className='logo'>LOGO</a>
           <div>
             <ul id='navbar' className={this.state.clicked ? '#navbar active': '#navbar'}>
               <li><a href='#'>Home</a></li>
@@ -39,24 +43,37 @@ class Header extends Component{
               <li><a href='#'>About Us</a></li>
               <li><a href='#'>Career</a></li>
               <li><a href='#'>Contact</a></li>
+
               <li id='language-two'>
-                <select  style={{padding:'10px 15px', border:'none', outline:'none', borderRadius:'5px'}}>          
-                   <option>English</option>
-                   <option>French</option>
-                </select>
+              <div  style={{background:'white', padding:'7px 15px', borderRadius:'5px'}}>
+                 <IoMdAdd style={{color:'red', fontSize:'20px'}}/>
+                 <select
+                   value={this.state.language}
+                   onChange={this.handleLanguageChange}
+                   style={{border:'none', outline:'none'}}
+                 >
+                    <option value="English">English</option>
+                    <option value="French">French</option>
+                 </select>
+              </div>
               </li>
+
             </ul>
           </div>
-              
-            <div id='mobile' 
-              onClick=
-              {this.handleClick}
-            >
-               <i id='bar'
-                  className=
-                  {this.state.clicked ? "fas fa-times": "fas fa-bars"}
-               ></i>
-            </div>
+           
+              <li id='language-one'>
+              <div id="language-dropdown" >
+                 <IoMdAdd style={{color:'red', fontSize:'20px'}}/>
+                 <select
+                   value={this.state.language}
+                   onChange={this.handleLanguageChange}
+                   style={{border:'none', outline:'none'}}
+                 >
+                    <option value="English">English</option>
+                    <option value="French">French</option>
+                 </select>
+              </div>
+              </li>
   
         
         </nav>
